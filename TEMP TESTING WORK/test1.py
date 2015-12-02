751ed52d8f9728b66027b4c504174419aa1864a9
BIGDADDY1974 <svilar@beotel.rs>
2015-12-02 12:19:49 +0100

    adding new pages to project

--- TEMP TESTING WORK/test1.py
+++ TEMP TESTING WORK/test1.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template,url_for
+from flask import Flask,request,render_template,url_for,redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'TWO' or request.form['password'] != 'FUNDUROS':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('homepage'))
+    return render_template('login.html', error=error)
+
+@app.route('/index')
 def homepage():
     return render_template("index.html")
 
@@ -13,6 +23,10 @@ def page1():
 @app.route('/page2')
 def page2():
     return render_template("page2.html")
+
+@app.route('/page3')
+def page3():
+    return render_template("page3.html")
 
 if __name__ == "__main__":
     app.run()
