87cee0418e6b149e1535153222d68de0e886af34
BIGDADDY1974 <svilar@beotel.rs>
2015-12-13 17:15:43 +0100

    REDIS IS WORKING, moving on

--- TEMP TESTING WORK/routes.py
+++ TEMP TESTING WORK/routes.py
@@ -1,14 +1,14 @@
 from flask import Flask, url_for, request, render_template;
 from app import app;
 import redis;
-
+r = redis.StrictRedis('localhost',6379,0);
 #Connect to redis data store
 #r = redis.StrictRedis(host='flaskmva.redis.cache.windows.net',port=6380,ssl=True,db=0, charset="utf-8", decode_responses=True, password='kxmLxHPfw5Xx8piaTlyv5VrPBBkKMoNTG6TNta+Pd5I=');
 # server/
 @app.route('/')
 def hello():
     #alternate ways to connect to redis, each command is equivalent
-    r = redis.StrictRedis();
+    #r = redis.StrictRedis();
     #r = redis.StrictRedis('localhost',6379,0);
     createLink = "<a href='" + url_for('create') + "'>Create a question</a>";
     return """<html>
@@ -31,16 +31,13 @@ def create():
         title = request.form['title'];
         question = request.form['question'];
         answer = request.form['answer'];
-
         # Store data in data store
         # Key name will be whatever title they typed in : Question
         # e.g. music:question countries:question
         # e.g. music:answer countries:answer
-
-
         # done :)
-        #r.set(title +':question', question)
-        #r.set(title +':answer',answer)
+        r.set(title +':question', question)
+        r.set(title +':answer', answer)
     
         return render_template('CreatedQuestion.html', question = question);
     else:
@@ -52,7 +49,7 @@ def question(title):
     if request.method == 'GET':
         # send the user the form
 
-        #question = r.get(title+':question')
+        question = r.get(title+':question')
         # Read question from data store
       
         return render_template('AnswerQuestion.html', question = question);
