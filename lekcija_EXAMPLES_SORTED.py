0f1442a039035aa4af2daebbaf6f4a5d074259af
BIGDADDY1974 <svilar@beotel.rs>
2015-12-21 18:40:10 +0100

    adding a new examples to the lekcija_EXAMPLES on classes

--- lekcija_EXAMPLES_SORTED.py
+++ lekcija_EXAMPLES_SORTED.py
@@ -3321,3 +3321,197 @@ print filter(lambda x: x % 3 == 0, my_list)
 #### LAMBDA FILTERING
 squares = [x**2 for x in range (1,11)]
 print filter(lambda x: x >= 30 and x <= 70, squares)
+
+#### CLASSES
+class Fruit(object):
+    """A class that makes various tasty fruits."""
+    def __init__(self, name, color, flavor, poisonous):
+        self.name = name
+        self.color = color
+        self.flavor = flavor
+        self.poisonous = poisonous
+
+    def description(self):
+        print "I'm a %s %s and I taste %s." % (self.color, self.name, self.flavor)
+
+    def is_edible(self):
+        if not self.poisonous:
+            print "Yep! I'm edible."
+        else:
+            print "Don't eat me! I am super poisonous."
+
+lemon = Fruit("lemon", "yellow", "sour", False)
+
+lemon.description()
+lemon.is_edible()
+
+##### CLASSES EXAMPLE SIMPLE
+class Animal(object):
+    def __init__(self,name):
+        self.name = name
+
+zebra = Animal("Jeffrey")
+print zebra.name
+
+##### ANOTHE CLASS EXAMPLE
+# Class definition
+class Animal(object):
+    """Makes cute animals."""
+    # For initializing our instance objects
+    def __init__(self, name, age, is_hungry):
+        self.name = name
+        self.age = age
+        self.is_hungry = is_hungry
+
+# Note that self is only used in the __init__()
+# function definition; we don't need to pass it
+# to our instance objects.
+
+zebra = Animal("Jeffrey", 2, True)
+giraffe = Animal("Bruce", 1, False)
+panda = Animal("Chad", 7, True)
+
+print zebra.name, zebra.age, zebra.is_hungry
+print giraffe.name, giraffe.age, giraffe.is_hungry
+print panda.name, panda.age, panda.is_hungry
+
+### CLASSES SCOPE
+class Animal(object):
+    """Makes cute animals."""
+    is_alive = True
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def description(self):
+        print self.name
+        print self.age
+
+hippo = Animal("Henry",10)
+cat = Animal("Boots",3)
+hippo.description
+print hippo.is_alive
+hippo.is_alive = False
+print cat.is_alive
+
+#### ANOTHER CLASS example
+ class Animal(object):
+    """Makes cute animals."""
+    is_alive = True
+    health = "good"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def description(self):
+        print self.name
+        print self.age
+
+hippo = Animal("Henry",10)
+hippo.description
+sloth = Animal("Pera",34)
+ocelot = Animal("Žika",45)
+print hippo.health
+print sloth.health
+print ocelot.health
+
+#### ANOTHER CLASS
+class ShoppingCart(object):
+    """Creates shopping cart objects
+    for users of our fine website."""
+    items_in_cart = {}
+    def __init__(self, customer_name):
+        self.customer_name = customer_name
+
+    def add_item(self, product, price):
+        """Add product to the cart."""
+        if not product in self.items_in_cart:
+            self.items_in_cart[product] = price
+            print product + " added."
+        else:
+            print product + " is already in the cart."
+
+    def remove_item(self, product):
+        """Remove product from the cart."""
+        if product in self.items_in_cart:
+            del self.items_in_cart[product]
+            print product + " removed."
+        else:
+            print product + " is not in the cart."
+my_cart = ShoppingCart("Davor Svilar")
+my_cart.add_item("Fruit",34)
+my_cart.add_item("Vegetable",56)
+print my_cart
+
+### CLASSES INHERITANCE
+class Customer(object):
+    """Produces objects that represent customers."""
+    def __init__(self, customer_id):
+        self.customer_id = customer_id
+
+    def display_cart(self):
+        print "I'm a string that stands in for the contents of your shopping cart!"
+
+class ReturningCustomer(Customer):
+    """For customers of the repeat variety."""
+    def display_order_history(self):
+        print "I'm a string that stands in for your order history!"
+
+monty_python = ReturningCustomer("ID: 12345")
+monty_python.display_cart()
+monty_python.display_order_history()
+
+#### ANOTHER CLASS EXAMPLE !!!
+class Employee(object):
+    """Models real-life employees!"""
+    def __init__(self, employee_name):
+        self.employee_name = employee_name
+
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 20.00
+
+class PartTimeEmployee(Employee):
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 12.00
+
+    def full_time_wage(self, hours):
+        self.hours = hours
+        return super(PartTimeEmployee, self).calculate_wage(hours)
+milton = PartTimeEmployee("milton")
+print milton.full_time_wage(10)
+
+##### ANOTHER CLASS example
+class Triangle(object):
+
+    number_of_sides = 3
+    def __init__(self, angle1, angle2, angle3):
+        self.angle1 = angle1
+        self.angle2 = angle2
+        self.angle3 = angle3
+
+    def check_angles(self):
+        sum = self.angle1 + self.angle2 + self.angle3
+        if sum == 180:
+            return True
+        else:
+            return false
+
+#### CLASSES EXAMPLE
+class Triangle(object):
+
+    number_of_sides = 3
+    def __init__(self, angle1, angle2, angle3):
+        self.angle1 = angle1
+        self.angle2 = angle2
+        self.angle3 = angle3
+
+    def check_angles(self):
+        sum = self.angle1 + self.angle2 + self.angle3
+        if sum == 180:
+            return True
+        else:
+            return false
+
+my_triangle = Triangle(90, 30, 60)
+print my_triangle.number_of_sides
+print my_triangle.check_angles()
