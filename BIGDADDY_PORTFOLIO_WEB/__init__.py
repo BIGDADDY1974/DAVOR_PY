c5ee20373b8155653792c0766a49e8bc0b69768c
BIGDADDY1974 <svilar@beotel.rs>
2016-01-11 15:26:48 +0100

    adding a new WEB base project WEB PORFOLIO to the Repo, This new dir has all the code and data for the complete web base project at davor.smallideafactory.com

---
+++ BIGDADDY_PORTFOLIO_WEB/__init__.py
@@ -0,0 +1,79 @@
+from flask import Flask, url_for, redirect, request, render_template;
+import redis;
+r = redis.StrictRedis('localhost',6379,0,charset="UTF-8",decode_responses=True);
+
+app = Flask(__name__)
+
+# This is a main route to main site Svilar davor portfolio
+@app.route('/')
+def portfolio():
+    return render_template('index.html')
+
+# This is a route to holistic site towards holistic.html
+@app.route('/holistic')
+def holistic():
+    return render_template('holistic.html')
+
+# This is a route to two funduros site towards login.html
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'TWO' or request.form['password'] != 'FUNDUROS':
+            error = 'Site is under construction, come again later !!!'
+        else:
+            return redirect(url_for('funduros'))
+    return render_template('login.html', error=error)
+
+@app.route('/funduros')
+def funduros():
+    return render_template("twofunduros.html")
+
+@app.route('/page1')
+def page1():
+    return render_template("page1.html")
+
+@app.route('/page2')
+def page2():
+    return render_template("page2.html")
+
+@app.route('/page3')
+def page3():
+    return render_template("page3.html")
+
+# This is a route to quiz site towards create question redis answer
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+
+        return render_template('CreateQuestion.html');
+    elif request.method == 'POST':
+
+        title = request.form['title'];
+        question = request.form['question'];
+        answer = request.form['answer'];
+        r.set(title +':question', question)
+        r.set(title +':answer', answer)
+        return render_template('CreatedQuestion.html', question = question,title = title, answer = answer);
+    else:
+        return "<h2>Invalid request</h2>";
+
+@app.route('/question/<title>', methods=['GET', 'POST'])
+def question(title):
+    if request.method == 'GET':
+        question = r.get(title+':question')
+        return render_template('AnswerQuestion.html', question = question,title = title);
+    elif request.method == 'POST':
+        submittedAnswer = request.form['submittedAnswer'];
+        answer = r.get(title+':answer')
+        if submittedAnswer == answer:
+            return render_template('Correct.html');
+        else:
+            return render_template('Incorrect.html', submittedAnswer = submittedAnswer, answer = answer);
+    else:
+        return '<h2>Invalid request</h2>';
+
+if __name__ == '__main__':
+    app.run()
+
--- lekcija_EXAMPLES_SORTED.py
+++ lekcija_EXAMPLES_SORTED.py
@@ -3861,3 +3861,109 @@ with open("text.txt", "w") as textfile:
 with open("text.txt", "w") as my_file:
 	my_file.write("I am the best there is :-)")
 
+#### PANDAS QUANDLE
+import Quandl
+import pandas as pd
+
+# Not necessary, I just do this so I do not show my API key.
+api_key = open('quandlapikey.txt','r').read()
+fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
+
+main_df = pd.DataFrame()
+
+for abbv in fiddy_states[0][0][1:]:
+    query = "FMAC/HPI_"+str(abbv)
+    df = Quandl.get(query, authtoken=api_key)
+
+    if main_df.empty:
+        main_df = df
+    else:
+        main_df = main_df.join(df)
+
+print (main_df.head())
+
+#### PANDAS PYTHON PICLKE
+import Quandl
+import pandas as pd
+import pickle
+
+# Not necessary, I just do this so I do not show my API key.
+api_key = open('quandlapikey.txt','r').read()
+
+def state_list():
+    fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
+    return fiddy_states[0][0][1:]
+
+
+def grab_initial_state_data():
+    states = state_list()
+
+    main_df = pd.DataFrame()
+
+    for abbv in states:
+        query = "FMAC/HPI_"+str(abbv)
+        df = Quandl.get(query, authtoken=api_key)
+        print(query)
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df)
+
+    pickle_out = open('fiddy_states.pickle','wb')
+    pickle.dump(main_df, pickle_out)
+    pickle_out.close()
+
+
+grab_initial_state_data()
+
+pickle_in = open('fiddy_states.pickle','rb')
+HPI_data = pickle.load(pickle_in)
+print(HPI_data)
+
+HPI_data.to_pickle('pickle.pickle')
+HPI_data2 = pd.read_pickle('pickle.pickle')
+print(HPI_data2)
+
+
+### PYTHON PANDA QUANDL PICKLE 2
+import Quandl
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('fivethirtyeight')
+
+# Not necessary, I just do this so I do not show my API key.
+api_key = open('quandlapikey.txt','r').read()
+
+def state_list():
+    fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
+    return fiddy_states[0][0][1:]
+
+def grab_initial_state_data():
+    states = state_list()
+
+    main_df = pd.DataFrame()
+
+    for abbv in states:
+        query = "FMAC/HPI_"+str(abbv)
+        df = Quandl.get(query, authtoken=api_key)
+        print(query)
+        df = df.pct_change()
+        print(df.head())
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df)
+
+    pickle_out = open('fiddy_states2.pickle','wb')
+    pickle.dump(main_df, pickle_out)
+    pickle_out.close()
+
+grab_initial_state_data()
+
+HPI_data = pd.read_pickle('fiddy_states2.pickle')
+
+HPI_data.plot()
+plt.legend().remove()
+plt.show()
