9e436f9958286cf0c14793187f669d8f2728c194
BIGDADDY1974 <svilar@beotel.rs>
2016-01-15 17:15:24 +0100

    adding a working example of davorsmallideafactory with new and improved web quiz wit MYSQL

--- BIGDADDY_PORTFOLIO_WEB/__init__.py
+++ BIGDADDY_PORTFOLIO_WEB/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, url_for, redirect, request, render_template;
+import MySQLdb;
 import redis;
-r = redis.StrictRedis('localhost',6379,0,charset="UTF-8",decode_responses=True);
 
 app = Flask(__name__)
+
+r = redis.StrictRedis('localhost',6379,0,charset="UTF-8",decode_responses=True);
 
 # This is a main route to main site Svilar davor portfolio
 @app.route('/')
@@ -74,8 +76,7 @@ def question(title):
     else:
         return '<h2>Invalid request</h2>';
 
-# This is a route to quiz site WHOS ZOUR DADDY
-
+# This is a route to quiz site WHOS YOUR DADDY
 @app.route("/main")
 def main():
     return render_template("main.html")
@@ -87,7 +88,90 @@ def wrong():
 @app.route("/right")
 def right():
     return render_template("right.html")
+# This is a route a WEB QZUIZ using SQL
+
+@app.route('/starter')
+def starter():
+    return render_template('starter.html');
+
+class Client(object):
+    def __init__(self):
+        self.connection = MySQLdb.connect("localhost","root","sv1l4r","quizdb");
+        self.cursor = self.connection.cursor();
+        return;
+    def saveQuestion(self, title, question, answer):
+        sql = "INSERT INTO questions VALUES ("+"'"+(title)+"',"+"'"+(question)+"',"+"'"+(answer)+"',"+"1"+")";
+        self.cursor.execute(sql);
+        self.connection.commit();
+        self.connection.close();
+        return;
+    def getQuestion(self, title):
+        sql = "SELECT Description FROM questions WHERE QuestionName = '"+(title)+"'";
+        self.cursor.execute(sql);
+        results = self.cursor.fetchone();
+        question = results;
+        self.connection.close()
+        for letter in results:
+            exit = ""
+            if letter != ("'"):
+                exit = exit + letter
+            else:
+                pass
+        return exit;
+    def getAnswer(self, title):
+        try:
+            sql = "SELECT CorrectAnswer FROM questions WHERE QuestionName = '"+(title)+"'";
+            self.cursor.execute(sql);
+            results = self.cursor.fetchone();
+            correctAnswer = results;
+            self.connection.close();
+            for letter in results:
+                exit = ""
+                if letter != ("'"):
+                    exit = exit + letter
+            else:
+                pass
+            return exit;
+        except MySQLdb.Error as err:
+            return err;
+
+# server/create
+@app.route('/create1', methods=['GET', 'POST'])
+def create1():
+    if request.method == 'GET':
+        return render_template('CreateQuestion1.html');
+    elif request.method == 'POST':
+        title = request.form['title'];
+        question = request.form['question'];
+        answer = request.form['answer'];
+
+        client = Client();
+        client.saveQuestion(title, question, answer);
+
+        return render_template('CreatedQuestion1.html', question = question,title = title);
+    else:
+        return "<h2>Invalid request</h2>";
+
+# server/question/<title>
+@app.route('/question1/<title>', methods=['GET', 'POST'])
+def question1(title):
+    if request.method == 'GET':
+        client = Client();
+        question = client.getQuestion(title);
+        return render_template('AnswerQuestion1.html', question = question);
+    elif request.method == 'POST':
+        submittedAnswer = request.form['submittedAnswer'];
+
+        client = Client();
+        answer = client.getAnswer(title)
+
+        if submittedAnswer == answer:
+            return render_template('Correct1.html');
+        else:
+            return render_template('Incorrect1.html', submittedAnswer = submittedAnswer, answer = answer);
+    else:
+        return '<h2>Invalid request</h2>';
+
 
 if __name__ == '__main__':
     app.run()
-
