88469ed83c2fca742654a372ea03befef5549b07
BIGDADDY1974 <svilar@beotel.rs>
2016-02-06 13:06:47 +0100

    addingnew code to the project DjangoTutorials,main page, contact, and right before the blog session

--- DjangoTutorials/mysite/webapp/urls.py
+++ DjangoTutorials/mysite/webapp/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.index, name='index')]
+    url(r'^$',views.index, name='index')
+
+]
 
 
 # Create your views here.
--- DjangoTutorials/mysite/webapp/views.py
+++ DjangoTutorials/mysite/webapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h2>HELLO THERE GOOD LOOKING !!!</h2>")
+    return HttpResponse("<h1>HELLO THERE GOOD LOOKING !!!</h1>")
 
 
 # Create your views here.
